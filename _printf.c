70b431bd2a97cbc973600ad3a2340de219f2bf55
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-26 16:38:23 -0700

    fixed missing include

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 		};
 
 	va_start(args, format);
-	if (format[0] == '%' && format[1] == '\0')
+	if (format == NULL)
 		return (-1);
 
 	while (format[i])
--- print_int.c
+++ print_int.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_int - Prints an integer
  * @args: argument
