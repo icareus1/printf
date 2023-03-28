eb77119767c62b806fe21c93881fe31d20448077
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-28 15:49:44 -0700

    made some changes

--- print_bin.c
+++ print_bin.c
@@ -8,24 +8,21 @@
 int print_bin(va_list args)
 {
 	unsigned int n = va_arg(args, unsigned int);
-	int i, j, bin_digit;
-	int len = 0;
-	char binary[32];
+	int i, len = 0; binary[32];
 
 	if (n == 0)
 	{
 		len += _putchar('0');
 		return (len);
 	}
-	for (i = 0; n > 0; i++)
+	for (; n > 0; i++)
 	{
-		binary[i] = n % 2 + '0';
+		binary[i] = (n % 2);
 		n /= 2;
 	}
-	for (j = i - 1; j >= 0; j--)
+	for (i = i - 1; i >= 0; i--)
 	{
-		bin_digit = binary[j];
-		len += _putchar(bin_digit);
+		len += _putchar(binary[i] + '0');
 	}
 	return (len);
 }
