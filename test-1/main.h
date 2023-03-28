abfb4773303100d8c72f1c97787271c19b5a7269
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-28 05:49:24 -0700

    test file

---
+++ test-1/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+/**
+* struct print -struct for print functions
+*
+* type: type to print
+* @func: print function
+*/
+typedef struct print
+{
+	char c;
+	int (*f)(va_list);
+} func_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+#endif
